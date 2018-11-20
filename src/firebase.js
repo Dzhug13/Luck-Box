1217bdcf30e9aa84d63dce1507f7729960d728a1
Dzhug13 <dzhugan@yahoo.com>
2018-11-21 00:56:26 +0200

    Luck Box Voting

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyAGpBRLcYewE4LHHyfwTabzikoCmWJC2XE",
+  authDomain: "lunch-rush-31e7b.firebaseapp.com",
+  databaseURL: "https://lunch-rush-31e7b.firebaseio.com",
+  projectId: "lunch-rush-31e7b",
+  storageBucket: "lunch-rush-31e7b.appspot.com",
+  messagingSenderId: "836040971789"
 };
 
 firebase.initializeApp(config);
